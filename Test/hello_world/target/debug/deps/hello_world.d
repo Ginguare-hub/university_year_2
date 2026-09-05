ae78df9058774de4d0a614d77c5f789a2bbbd66b
Ginguare <ginguare@gmail.com>
2026-09-05 10:31:53 +0300

    first rust hello world

---
+++ Test/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\GitHub\university_year_2\Test\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+C:\GitHub\university_year_2\Test\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+src\main.rs:
---
+++ Test/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\GitHub\university_year_2\Test\hello_world\target\debug\hello_world.exe: C:\GitHub\university_year_2\Test\hello_world\src\main.rs
